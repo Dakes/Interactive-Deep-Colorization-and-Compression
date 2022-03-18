6aba020219ce6c245406c1fa0d83dd2b4d486c2b
Daniel Ostertag <dakes@vivaldi.net>
2022-03-18 02:22:03 +0100

    Bugfixes, import, config path, png compression

--- src/dinterface/preprocess.py
+++ src/dinterface/preprocess.py
@@ -2,12 +2,15 @@
 Calling this file from commandline, preprocesses all images with default parameters
 """
 
-import os
+import os, sys
 import cv2
 import numpy as np
 
-from src.dinterface.utils import add_color_pixels_rand_gt, imsegkmeans
-import src.utils.files as files
+sys.path.insert(1, os.path.join(sys.path[0], '../..'))
+from utils import add_color_pixels_rand_gt, imsegkmeans
+# from src.dinterface.utils import add_color_pixels_rand_gt, imsegkmeans
+from src.utils import files
+# import src.utils.files as files
 
 
 def get_fn_wo_ext(fn):
@@ -15,15 +18,16 @@ def get_fn_wo_ext(fn):
         fn = os.path.splitext(fn)[0]
     return fn
 
-def theme_gen_save(dirs, img, filename, num_points_theme=6):
+def theme_gen_save(dirs, img, filename, num_points_theme=6, save_segmented=False):
     """
     Generates global theme and saves to disk. (to paths in dir)
     """
     segmented_img, theme, mask = imsegkmeans(img, num_points_theme)
     filename = get_fn_wo_ext(filename)
-    cv2.imwrite(dirs["color_map"] + filename + '.png', segmented_img)
-    cv2.imwrite(dirs["theme_rgb"] + filename + '.png', theme)
-    cv2.imwrite(dirs["theme_mask"] + filename + '.png', mask)
+    if save_segmented:
+        cv2.imwrite(dirs["color_map"] + filename + '.png', segmented_img)
+    cv2.imwrite(dirs["theme_rgb"] + filename + '.png', theme, [int(cv2.IMWRITE_PNG_COMPRESSION),0])
+    cv2.imwrite(dirs["theme_mask"] + filename + '.png', mask, [int(cv2.IMWRITE_PNG_COMPRESSION),0])
 
 def local_gen_save(dirs, img, filename, num_points_pix):
     """
@@ -31,10 +35,11 @@ def local_gen_save(dirs, img, filename, num_points_pix):
     """
     points_mask, points_rgb = add_color_pixels_rand_gt(img, num_points_pix)
     filename = get_fn_wo_ext(filename)
-    cv2.imwrite(dirs["local_mask"] + filename + '.png', points_mask)
-    cv2.imwrite(dirs["local_hints"] + filename + '.png', points_rgb)
+    cv2.imwrite(dirs["local_mask"] + filename + '.png', points_mask, [int(cv2.IMWRITE_PNG_COMPRESSION),0])
+    cv2.imwrite(dirs["local_hints"] + filename + '.png', points_rgb, [int(cv2.IMWRITE_PNG_COMPRESSION),0])
 
 def crop(img, random_crop=256):
+    # TODO: save crops (needed for dynamic point selection). (Except seed is used [derived from filename?])
     h, w, _ = img.shape
     max_sample_h = h - random_crop
     max_sample_w = w - random_crop
@@ -45,25 +50,33 @@ def crop(img, random_crop=256):
 
 def convert_grayscale(dirs, img):
     # TODO: implement
-    pass
+    return img
 
 # TODO: later (joint training) use same crop for every file (same random seed??)
 def preprocess_grayscale(dirs, random_crop=256):
+    # NOTE: Naaah, thats stupid, convert to grayscale on demand. Is quicker than saving and loading twice
+    # (Unless we use a ramdisk to save the 140gb dataset, which we can totally do *flex*)
     if not dirs:
         dirs = files.config_parse(dirs=True)
+
+    counter = 0
     for dirpath, dnames, fnames in os.walk(dirs["original_img"]):
         for file in fnames:
             if file.lower().endswith(('.png', '.jpg', '.jpeg')):
+                counter += 1
+                if not counter % 10000:
+                    print("preprocess_grayscale; Processed", counter, "images")
                 temp_fn = get_fn_wo_ext(file)
                 item_path = os.path.join(dirpath, file)
                 img = cv2.imread(item_path)
                 if random_crop:
                     img = crop(img, random_crop)
-                # TODO: implement
-                img = convert_grayscale(dirs, img)
-                cv2.imwrite(dirs["original_img"] + temp_fn + '.png', img)
 
-def preprocess_color(dirs, num_points_pix, num_points_theme, random_crop=256, align=False, only_locals=False):
+                img = convert_grayscale(dirs, img)  # TODO: implement
+                # cv2.imwrite(dirs["original_gray_img"] + temp_fn + '.png', img, [int(cv2.IMWRITE_PNG_COMPRESSION),0])
+
+def preprocess_color(dirs, num_points_pix, num_points_theme, random_crop=256, align=False, only_locals=False,
+                     save_segmented=False):
     """
     clean python implementation of:
     https://github.com/praywj/Interactive-Deep-Colorization-and-Compression/tree/master/prepare_dataset
@@ -80,9 +93,14 @@ def preprocess_color(dirs, num_points_pix, num_points_theme, random_crop=256, al
     if not dirs:
         dirs = files.config_parse(dirs=True)
 
+    counter = 0
     for dirpath, dnames, fnames in os.walk(dirs["original_img"]):
         for file in fnames:
             if file.lower().endswith(('.png', '.jpg', '.jpeg')):
+                counter += 1
+                print(file)
+                if not counter % 1000:
+                    print("preprocess_color; Processed", counter, "images")
                 temp_fn = get_fn_wo_ext(file)
 
                 item_path = os.path.join(dirpath, file)
@@ -97,14 +115,41 @@ def preprocess_color(dirs, num_points_pix, num_points_theme, random_crop=256, al
 
                 local_gen_save(dirs, img, temp_fn, num_points_pix)
                 if not only_locals:
-                    theme_gen_save(dirs, img, temp_fn, num_points_theme)
-                    cv2.imwrite(dirs["original_img"] + temp_fn + '.png', img)
+                    theme_gen_save(dirs, img, temp_fn, num_points_theme, save_segmented)
+                    # save alined?? But not like this
+                    # cv2.imwrite(dirs["original_img"] + temp_fn + '.png', img, [int(cv2.IMWRITE_PNG_COMPRESSION),0])
 
 
+def dataset_prepare(dirs):
+    """
+    Links dataset into 'original_img'. Runs recursively.
+    ALL images below given folder will be used.
+    REMOVES all old links
+    """
+    # remove all old links
+    print("Deleting old symlinks in:", dirs["original_img"])
+    for dirpath, _, fnames in os.walk(dirs["original_img"]):
+        for file in fnames:
+            link = os.path.join(dirpath, file)
+            if os.path.islink(link):
+                os.unlink(link)
+
+    counter = 0
+    for dirpath, _, fnames in os.walk(dirs["dataset"]):
+        for file in fnames:
+            if file.lower().endswith(('.png', '.jpg', '.jpeg')):
+                counter += 1
+                if not counter % 10000:
+                    print("dataset_prepare; Symlinked", counter, "images")
+                src_file = os.path.join(dirpath, file)
+                new_link = os.path.join(dirs["original_img"], file)
+                os.symlink(src_file, new_link)
 
 def main():
-    preprocess_color(dirs=None, num_points_pix=-1, num_points_theme=-1, random_crop=False)
-    preprocess_grayscale(dirs=None, random_crop=False)
+    dirs = files.config_parse(dirs=True)
+    dataset_prepare(dirs)
+    preprocess_color(dirs, num_points_pix=-1, num_points_theme=-1, random_crop=False)
+    preprocess_grayscale(dirs, random_crop=False)
 
 
 if __name__ == '__main__':
